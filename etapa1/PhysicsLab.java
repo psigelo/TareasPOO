0664d4f668fec6bd97074ec42de4de24011a9a16
Pascal Sigel <pascal.sigel@gmail.com>
2014-04-05 01:35:04 -0300

    Se ha agregado la opcion de agregar los datos a traves de teclado en etapa1

--- etapa1/PhysicsLab.java
+++ etapa1/PhysicsLab.java
@@ -1,12 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Locale;
+
 public class PhysicsLab {
    public static void main(String[] args) {
+    double deltaTime;
+    double endTime;
+    double samplingTime;
+
       if (args.length != 3)  {
-        System.out.println("usage: java PhysicsLab <delta_time[s]> <end_time[s]> <sampling_time[s]>");
-        System.exit(-1);
+        Scanner s = new Scanner(System.in).useLocale(Locale.US);
+        System.out.print("delta (ejemplo 0.001): ");
+        deltaTime = s.nextDouble();
+        System.out.print("Duracion experimento (ejemplo 5.0): ");
+        endTime = s.nextDouble();
+        System.out.print("Tiempo de muestreo (ejemplo 0.001): ");
+        samplingTime = s.nextDouble();
       }
-      double deltaTime      = Double.parseDouble(args[0]);      // [s]
-      double endTime        = Double.parseDouble(args[1]);      // [s]
-      double samplingTime   = Double.parseDouble(args[2]);      // [s]
+      else{
+        deltaTime      = Double.parseDouble(args[0]);      // [s]
+        endTime        = Double.parseDouble(args[1]);      // [s]
+        samplingTime   = Double.parseDouble(args[2]);      // [s]
+      }
       MyWorld world         = new MyWorld(System.out);
       
       double mass           = 1.0;                              // 1 [kg] 
