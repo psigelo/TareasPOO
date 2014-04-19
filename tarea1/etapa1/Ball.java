fa9d299400a89bd596cefa7b93323b8869de1ab1
Javier Cabezas <jcleyton@gmail.com>
2014-04-18 21:58:58 -0300

    Comentarios e identación de la primera parte.

--- tarea1/etapa1/Ball.java
+++ tarea1/etapa1/Ball.java
@@ -1,64 +1,70 @@
 import java.util.*;
 
 public class Ball extends PhysicsElement {
-   private static int id=0;  // Ball identification number
-   private final double mass;
-   private final double radius;
-   private double pos_t;     // current position at time t
-   private double pos_tPlusDelta;  // next position in delta time in future
-   private double speed_t;   // speed at time t
-   private double speed_tPlusDelta;   // speed in delta time in future
-   
-   private Ball(){   // nobody can create a block without state
-     this(1.0,0.1,0,0);
-   }
-   public Ball(double mass, double radius, double position, double speed){
-      super(id++);
-      this.mass = mass;
-      pos_t = position;
-      speed_t = speed;
-      this.radius = radius;
-   }
-   public double getRadius() {
-      return radius;
-   }
-   public double getSpeed() {
-      return speed_t;
-   }
-   public void computeNextState(double delta_t, MyWorld world) {
-     Ball b;  // Assumption: on collision we only change speed.   
-     if ((b=world.findCollidingBall(this))!= null){ /* elastic collision */
-        speed_tPlusDelta=(speed_t*(mass-b.getMass())+2*b.getMass()*b.getSpeed())/(mass+b.getMass());
-        pos_tPlusDelta = pos_t + speed_tPlusDelta*delta_t;
-     } else {
-        pos_tPlusDelta = pos_t + speed_t*delta_t;
-        speed_tPlusDelta = speed_t;
-     }
-   }
-   public boolean collide(Ball b) {
-   // to be coded by you
-    if     ( Math.abs(pos_t + radius - (b.pos_t - b.radius))  <  0.001*radius )  return true; 
-    else if( Math.abs(pos_t - radius - (b.pos_t + b.radius))  <  0.001*radius )  return true;
-    return false;
-  }
+    private static int id=0;  // Ball identification number
+    private final double mass;
+    private final double radius;
+    private double pos_t;     // current position at time t
+    private double pos_tPlusDelta;  // next position in delta time in future
+    private double speed_t;   // speed at time t
+    private double speed_tPlusDelta;   // speed in delta time in future
+    
+    private Ball(){   // nobody can create a block without state
+        this(1.0,0.1,0,0);
+    }
 
-   public void updateState(){
-     pos_t    = pos_tPlusDelta;
-     speed_t  = speed_tPlusDelta;
-   }
-   
-   public String getDescription() {
-   // to be coded by you
-    String imprimir      = new String("Ball");
-    imprimir            += this.getId() + ":x";
-    return imprimir;
-   }
-
-   public void printState(){
-    System.out.format(Locale.US,"%.5f", pos_t);
-   }
-
-   public double getMass(){
-    return mass;
-   }
+    public Ball(double mass, double radius, double position, double speed){
+        super(id++);
+        this.mass = mass;
+        pos_t = position;
+        speed_t = speed;
+        this.radius = radius;
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+    
+    public double getSpeed() {
+        return speed_t;
+    }
+    
+    public void computeNextState(double delta_t, MyWorld world) {
+        Ball b;  // Assumption: on collision we only change speed.
+        if ((b=world.findCollidingBall(this))!= null){ 
+            // Colisión elástica.
+            speed_tPlusDelta=(speed_t*(mass-b.getMass())+2*b.getMass()*b.getSpeed())/(mass+b.getMass());
+            pos_tPlusDelta = pos_t + speed_tPlusDelta*delta_t;
+        } else {
+            //Sin colisión : actualizar variables de forma regular.
+            pos_tPlusDelta = pos_t + speed_t*delta_t;
+            speed_tPlusDelta = speed_t;
+        }
+    }
+    public boolean collide(Ball b) {
+        if     ( Math.abs(pos_t + radius - (b.pos_t - b.radius))  <  0.001*radius )  
+            return true; //Colisión de la parte de adelante de una de las pelotas con la parte de atrás de otra.
+        else if( Math.abs(pos_t - radius - (b.pos_t + b.radius))  <  0.001*radius )  
+            return true; //El caso contrario.
+        return false; //Sin colisión.
+    }
+    
+    public void updateState(){
+        pos_t    = pos_tPlusDelta;
+        speed_t  = speed_tPlusDelta;
+    }
+    
+    public String getDescription() {
+        String imprimir      = new String("Ball");
+        imprimir            += this.getId() + ":x";
+        return imprimir;
+    }
+    
+    public void printState(){
+        System.out.format(Locale.US,"%.5f", pos_t);
+    }
+    
+    public double getMass(){
+        return mass;
+    }
 }
--- tarea1/etapa1/PhysicsLab.java
+++ tarea1/etapa1/PhysicsLab.java
@@ -5,36 +5,34 @@ import javax.swing.*;
 
 
 public class PhysicsLab {
-   public static void main(String[] args) {
-    double deltaTime;
-    double endTime;
-    double samplingTime;
-
-      if (args.length != 3)  {
+    public static void main(String[] args) {
+        double deltaTime;
+        double endTime;
+        double samplingTime;
         
-        String Sdelta     = JOptionPane.showInputDialog("Introduzca delta: ");
-        String Send       = JOptionPane.showInputDialog("Introduzca duracion: ");
-        String Ssampling  = JOptionPane.showInputDialog("Introduzca tiempo_muestreo: ");
-        deltaTime         = Double.parseDouble(Sdelta);
-        endTime           = Double.parseDouble(Send);
-        samplingTime      = Double.parseDouble(Ssampling);
+        if (args.length != 3)  {
+            String Sdelta     = JOptionPane.showInputDialog("Introduzca delta: ");
+            String Send       = JOptionPane.showInputDialog("Introduzca duracion: ");
+            String Ssampling  = JOptionPane.showInputDialog("Introduzca tiempo_muestreo: ");
+            deltaTime         = Double.parseDouble(Sdelta);
+            endTime           = Double.parseDouble(Send);
+            samplingTime      = Double.parseDouble(Ssampling);
+        }
+        else{
+            deltaTime      = Double.parseDouble(args[0]);      // [s]
+            endTime        = Double.parseDouble(args[1]);      // [s]
+            samplingTime   = Double.parseDouble(args[2]);      // [s]
+        }
 
-      }
-      else{
-        deltaTime      = Double.parseDouble(args[0]);      // [s]
-        endTime        = Double.parseDouble(args[1]);      // [s]
-        samplingTime   = Double.parseDouble(args[2]);      // [s]
-      }
-      MyWorld world         = new MyWorld(System.out);
-      
-      double mass           = 1.0;                              // 1 [kg] 
-      double radius         = 0.1;                              // 10 [cm] 
-      double position       = 1.0;                              // 1 [m] 
-      double speed          = 0.5;                              // 0.5 [m/s]
-      Ball b0               = new Ball(mass, radius, position, speed);
-      Ball b1               = new Ball(mass, radius, 2.56, 0);
-      world.addElement(b0);
-      world.addElement(b1);
-      world.simulate(deltaTime, endTime, samplingTime);         // delta time[s], total simulation time [s].
-   }
+        MyWorld world         = new MyWorld(System.out);
+        double mass           = 1.0;                              // 1 [kg]
+        double radius         = 0.1;                              // 10 [cm]
+        double position       = 1.0;                              // 1 [m]
+        double speed          = 0.5;                              // 0.5 [m/s]
+        Ball b0               = new Ball(mass, radius, position, speed);
+        Ball b1               = new Ball(mass, radius, 2.56, 0);
+        world.addElement(b0);
+        world.addElement(b1);
+        world.simulate(deltaTime, endTime, samplingTime);         // delta time[s], total simulation time [s].
+    }
 }
