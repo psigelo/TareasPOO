7ea8a5a307a45b8acc7ef9c55006639944a64bff
Javier Cabezas <jcleyton@gmail.com>
2014-05-27 22:41:51 -0400

    Fin de javadoc.

--- tarea2/etapa5/BallView.java
+++ tarea2/etapa5/BallView.java
@@ -7,6 +7,11 @@ public class BallView {
 	private Ball ball;
 	private boolean isSelected;
 	
+        /**
+         * Asigna los valores que tiene una ball en específico a una vista.
+         * 
+         * @param b la ball a la cual se le conseguirán los parámetros.
+         */
 	public BallView (Ball b){
 		Double x = b.getPosition();
 		Double y = 0.0;
@@ -17,23 +22,47 @@ public class BallView {
 		isSelected = false;
 	}
 	
+        /**
+         * Revisa si los parámetros entregados por argumento pertecen al objeto ball.
+         * 
+         * @param x la posición x a revisar
+         * @param y la posición y a revisar
+         * @return true si el ball está en la coordenada (x, y) entregada, false en caso contrario.
+         */
 	public boolean contains (double x, double y){
 		return shape.contains(x,y);
 	}
+        
+        /**
+         * Selecciona al ball en específico y lo deja de color cyan para identificarlo.
+         */
 	public void setSelected (){
 		color = Color.CYAN;
 		isSelected= true;
 	}
 	
+        /**
+         * Des-selecciona al ball y lo deja color azul.
+         */
 	public void setReleased() {
 		color = Color.BLUE;
 		isSelected= false;
 	}
-
+        
+        /**
+         * Retorna el estado de la selección del ball.
+         * 
+         * @return true si está selecionado y false en caso contrario.
+         */
 	public boolean getIsSelected(){
 		return isSelected;
 	}
 	
+        /**
+         * Actualiza la vista de la ball en el elemento gráfico.
+         * 
+         * @param g el elemento gráfico en donde se actualizará la vista.
+         */
 	public void updateView(Graphics2D g) {
 		double radius = ball.getRadius();
 		shape.setFrame(ball.getPosition()-radius, -radius, 2*radius, 2*radius);
--- tarea2/etapa5/MyWorld.java
+++ tarea2/etapa5/MyWorld.java
@@ -20,6 +20,12 @@ public class MyWorld implements ActionListener {
     public MyWorld(){
         this(System.out);  // delta_t= 0.1[ms] and refreshPeriod=200 [ms]
     }
+
+    /**
+     * Clase inicial que asigna los parámetros por defecto.
+     * 
+     * @param output 
+     */
     public MyWorld(PrintStream output){
         out = output;
         t = 0;
@@ -30,28 +36,59 @@ public class MyWorld implements ActionListener {
         passingTime = new Timer((int)(refreshPeriod*1000), this);
     }
     
+    /**
+     * Añade un elemento físico a la vista y además actualiza la misma.
+     * 
+     * @param e el elemento físico a agregar 
+     */
     public void addElement(PhysicsElement e) {
         elements.add(e);
         view.repaintView();
     }
+
     public void setView(MyWorldView view) {
         this.view = view;
     }
+    
+    /**
+     * Asigna un valor de delta al mundo.
+     * 
+     * @param delta el valor de delta nuevo a utilizar.
+     */
     public void setDelta_t(double delta) {
         delta_t = delta;
     }
+    
+    /**
+     * Asigna un nuevo valor de refresco al mundo.
+     * 
+     * @param rp el nuevo valor de refresco nuevo a utilizar.
+     */
     public void setRefreshPeriod (double rp) {
         refreshPeriod = rp;
         passingTime.setDelay((int)(refreshPeriod*1000)); // convert from [s] to [ms]
     }
+    
+    /**
+     * Inicia el paso del tiempo en caso de que no esté pasando ya.
+     */
     public void start() {
         if(passingTime.isRunning()) return;
         passingTime.start();
     }
+    
+    /**
+     * Detiene el paso del tiempo.
+     */
     public void stop(){
         passingTime.stop();
     }
     
+    /**
+     * Llama a computar los estados físicos de los elementos que estén en el mundo y 
+     *
+     * @param event es un evento generado por timer pero que no se utiliza en el método.
+     */
     public void actionPerformed (ActionEvent event) {  // like simulate method of Assignment 1,
         double nextStop=t+refreshPeriod;                // the arguments are attributes here.
         for (; t<nextStop; t+=delta_t){
@@ -69,10 +106,19 @@ public class MyWorld implements ActionListener {
         repaintView();
     }
     
+    /**
+     * Llama al método repaintView para actualizar la vista del mundo.
+     */
     public void repaintView(){
         view.repaintView();
     }
     
+    /**
+     * Revisa si el elemento entregado por argumento es instancia de colisionable.
+     * 
+     * @param me el elemento a revisar
+     * @return nulo en caso de que no sea colisionable y el elemento físico en caso de que lo sea.
+     */
     public Collidable findCollidingElement(PhysicsElement me) {
         Collidable coli;
         for (PhysicsElement e: elements)
@@ -85,20 +131,27 @@ public class MyWorld implements ActionListener {
         return null;
     }
     
-    
+    /**
+     * Llama al método para actualizar la vista de todos los elementos físicos del mundo.
+     * 
+     * @param g el elemento gráfico en donde se actualizarán los elementos.
+     */
     public void updateView(Graphics2D g){
         for (PhysicsElement e: elements)
             e.updateView(g);
     }
     
-    
+    /**
+     * Selecciona los elementos físicos que estén en la posición entregada por argumento.
+     * 
+     * @param x posición x a revisar.
+     * @param y posición y a revisar.
+     */
     public void findSelection(double x, double y){
         if(!passingTime.isRunning()){
             for (PhysicsElement e: elements){
-                
-                
                 if( e.contains(x, y) )
-                e.setSelected();
+                    e.setSelected();
                 else{
                     e.setReleased();
                 }
@@ -107,6 +160,11 @@ public class MyWorld implements ActionListener {
         }
     }
     
+    /**
+     * Mueve el elemento físico a una nueva posición.
+     * @param x posición en el eje de las x
+     * @param y posición en el eje de las y
+     */
     public void moveSelection(double x, double y){
         if(!passingTime.isRunning()){
             for (PhysicsElement e: elements){
@@ -118,6 +176,11 @@ public class MyWorld implements ActionListener {
         }
     }
     
+    /**
+     * Revisa si al word le está transcurriendo el tiempo.
+     * 
+     * @return true si es que está corriendo y false en caso contrario. 
+     */
     public boolean getIsRunning(){
         return passingTime.isRunning();
     }
